08f7a70b394ea21e397dfa4bafe35b5e278313ca
HyeongJun <kael21ce@kaist.ac.kr>
2023-09-19 11:36:18 +0900

    Correct error where bottomNavigation does not appear

--- app/src/main/java/com/kael21ce/sleepanalysisandroid/MainActivity.java
+++ app/src/main/java/com/kael21ce/sleepanalysisandroid/MainActivity.java
@@ -1,7 +1,11 @@
 package com.kael21ce.sleepanalysisandroid;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,36 +18,41 @@ public class MainActivity extends AppCompatActivity {
     RecommendFragment recommendFragment = new RecommendFragment();
     SettingFragment settingFragment = new SettingFragment();
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        getSupportFragmentManager().beginTransaction().add(R.id.mainFrame, homeFragment);
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, homeFragment);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(
-                item -> {
-                    switch (item.getItemId()) {
-                        case 1000004:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame,
-                                    homeFragment).commit();
-                            break;
-                        case 1000002:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame,
-                                    scheduleFragment).commit();
-                            break;
-                        case 1000007:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame,
-                                    recommendFragment).commit();
-                            break;
-                        case 1000003:
-                            getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame,
-                                    settingFragment).commit();
-                            break;
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        if (item.getItemId() == R.id.tabHome) {
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.mainFrame, homeFragment).commit();
+                            return true;
+                        } else if (item.getItemId() == R.id.tabSchedule) {
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.mainFrame, scheduleFragment).commit();
+                            return true;
+                        } else if (item.getItemId() == R.id.tabRecommend) {
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.mainFrame, recommendFragment).commit();
+                            return true;
+                        } else if (item.getItemId() == R.id.tabSetting) {
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.mainFrame, settingFragment).commit();
+                            return true;
+                        } else {
+                            return false;
+                        }
                     }
-                    return true;
                 }
         );
+
     }
 }
