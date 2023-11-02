eec00da9a88c1cc684547f0234c868909df773fb
reinattwijaya <71814354+reinattwijaya@users.noreply.github.com>
2023-11-03 06:02:34 +0900

    add layout for setting sleep onset

--- app/src/main/java/com/kael21ce/sleepanalysisandroid/SettingFragment.java
+++ app/src/main/java/com/kael21ce/sleepanalysisandroid/SettingFragment.java
@@ -8,6 +8,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.kael21ce.sleepanalysisandroid.data.Sleep;
 
 public class SettingFragment extends Fragment {
 
@@ -15,6 +18,11 @@ public class SettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_setting, container, false);
+        LinearLayout onsetView = v.findViewById(R.id.OnsetView);
+        onsetView.setOnClickListener(view -> {
+            SleepOnsetFragment sleepOnsetFragment = new SleepOnsetFragment();
+            getChildFragmentManager().beginTransaction().replace(R.id.SettingView, sleepOnsetFragment).commit();
+        });
         return v;
     }
 }
