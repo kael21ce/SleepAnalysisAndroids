fddfca730c8b9c5bc3254fef0526073cd77fae8e
reinattwijaya <71814354+reinattwijaya@users.noreply.github.com>
2023-11-06 02:23:48 +0900

    update activity reload

--- app/src/main/java/com/kael21ce/sleepanalysisandroid/SleepOnsetFragment.java
+++ app/src/main/java/com/kael21ce/sleepanalysisandroid/SleepOnsetFragment.java
@@ -1,10 +1,14 @@
 package com.kael21ce.sleepanalysisandroid;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -140,6 +144,8 @@ public class SleepOnsetFragment extends Fragment implements ButtonTextUpdater{
             mainActivity.setWorkOnset(workOnset.getTime());
             mainActivity.setWorkOffset(workOffset.getTime());
 
+            mainActivity.finish();
+            startActivity(new Intent(mainActivity, SplashActivity.class));
         });
 
         // Inflate the layout for this fragment
