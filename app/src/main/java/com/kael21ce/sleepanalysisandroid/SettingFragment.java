0fc7db7db4802e690af2353a2f730abe68695245
HyeongJun <kael21ce@kaist.ac.kr>
2023-11-24 18:48:19 +0900

    Make OnsetView foldable in SettingFragment

--- app/src/main/java/com/kael21ce/sleepanalysisandroid/SettingFragment.java
+++ app/src/main/java/com/kael21ce/sleepanalysisandroid/SettingFragment.java
@@ -1,27 +1,44 @@
 package com.kael21ce.sleepanalysisandroid;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.kael21ce.sleepanalysisandroid.data.Sleep;
 
 public class SettingFragment extends Fragment {
 
+    Boolean isFolded = true;
+    SleepOnsetFragment sleepOnsetFragment = new SleepOnsetFragment();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_setting, container, false);
+
         LinearLayout onsetView = v.findViewById(R.id.OnsetView);
+        ImageView onsetButton = v.findViewById(R.id.OnsetButton);
         onsetView.setOnClickListener(view -> {
-            SleepOnsetFragment sleepOnsetFragment = new SleepOnsetFragment();
-            getChildFragmentManager().beginTransaction().replace(R.id.SettingView, sleepOnsetFragment).commit();
+            if (isFolded) {
+                isFolded = false;
+                getChildFragmentManager().beginTransaction().replace(R.id.SettingView, sleepOnsetFragment).commit();
+                onsetView.setBackground(AppCompatResources.getDrawable(v.getContext(), R.color.gray_1));
+                ObjectAnimator.ofFloat(onsetButton, View.ROTATION, 0f, 90f).setDuration(100).start();
+            } else {
+                isFolded = true;
+                getChildFragmentManager().beginTransaction().remove(sleepOnsetFragment).commit();
+                onsetView.setBackground(AppCompatResources.getDrawable(v.getContext(), R.color.white));
+                ObjectAnimator.ofFloat(onsetButton, View.ROTATION, 90f, 0f).setDuration(100).start();
+            }
         });
         return v;
     }
