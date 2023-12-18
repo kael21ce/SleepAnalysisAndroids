a21273e7b20df7c1d592b9094254602c2cd59e59
HyeongJun <kael21ce@kaist.ac.kr>
2023-12-18 11:12:09 +0900

    Add ticks to clock

--- app/src/main/java/com/kael21ce/sleepanalysisandroid/ClockView.java
+++ app/src/main/java/com/kael21ce/sleepanalysisandroid/ClockView.java
@@ -3,6 +3,7 @@ package com.kael21ce.sleepanalysisandroid;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -18,8 +19,11 @@ public class ClockView extends View {
     private float sweepAngle = 270;
     private Paint arcPaint;
     private Paint circlePaint;
+    private Paint tickPaint;
+    private Paint textPaint;
     //To distinguish the type of interval
     private int typeOfInterval = 1;
+    private final Rect textBounds = new Rect();
 
     public ClockView(Context context) {
         super(context);
@@ -45,10 +49,19 @@ public class ClockView extends View {
         } else {
             arcPaint.setColor(getResources().getColor(R.color.blue_1, null));
         }
+        tickPaint = new Paint();
+        tickPaint.setStyle(Paint.Style.FILL);
+        tickPaint.setColor(getResources().getColor(R.color.gray_1, null));
 
         circlePaint = new Paint();
         circlePaint.setStyle(Paint.Style.FILL);
         circlePaint.setColor(getResources().getColor(R.color.white, null));
+
+        textPaint = new Paint();
+        textPaint.setTextSize(40);
+        textPaint.setTypeface(getResources().getFont(R.font.pretendardbold));
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setColor(getResources().getColor(R.color.gray_2, null));
 
         int width = getWidth();
         int height = getHeight();
@@ -84,6 +97,34 @@ public class ClockView extends View {
         double circleRadius = Math.min(width, height) / 2.8;
         float CircleRadius = (float) circleRadius;
         canvas.drawCircle(centerX, centerY, CircleRadius, circlePaint);
+        //Tick arc
+        float tickRadius = CircleRadius - 5f;
+        float tickLeft = centerX - tickRadius;
+        float tickTop = centerY - tickRadius;
+        float tickRight = centerX + tickRadius;
+        float tickBottom = centerY + tickRadius;
+        for (int i = 0; i < 24; i++) {
+            float startTickAngle = 15*i - 0.729f;
+            float sweepTickAngle = 1.458f;
+            canvas.drawArc(tickLeft, tickTop, tickRight, tickBottom, startTickAngle, sweepTickAngle,
+                    true, tickPaint);
+        }
+        //Draw circle to show part of tick arc
+        float tickCircleRadius = CircleRadius - 40f;
+        canvas.drawCircle(centerX, centerY, tickCircleRadius, circlePaint);
+        //Text for time
+        float textRadius = tickCircleRadius - 20f;
+        drawTextCentred(canvas, textPaint, "0", centerX, centerY - textRadius);
+        drawTextCentred(canvas, textPaint, "6", centerX + textRadius, centerY);
+        drawTextCentred(canvas, textPaint, "12", centerX, centerY + textRadius);
+        drawTextCentred(canvas, textPaint, "18", centerX - textRadius, centerY);
+
+        /*
+        canvas.drawText("0", centerX, centerY - textRadius + 12.5f, textPaint);
+        canvas.drawText("6", centerX + textRadius, centerY + 12.5f, textPaint);
+        canvas.drawText("12", centerX, centerY + textRadius + 12.5f, textPaint);
+        canvas.drawText("18", centerX - textRadius, centerY + 12.5f, textPaint);
+         */
     }
 
     //Set the start Time and end Time
@@ -141,6 +182,12 @@ public class ClockView extends View {
 
             return result;
         }
+    }
+
+    //Draw text with horizontal & vertical alignment
+    public void drawTextCentred(Canvas canvas, Paint paint, String text, float cx, float cy) {
+        paint.getTextBounds(text, 0, text.length(), textBounds);
+        canvas.drawText(text, cx, cy - textBounds.exactCenterY(), paint);
     }
 }
 
