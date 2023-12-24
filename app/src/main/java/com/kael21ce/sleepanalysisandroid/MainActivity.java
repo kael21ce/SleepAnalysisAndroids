2d389a5667d5bfaf1c260af67614b52bc19d21c6
HyeongJun <kael21ce@kaist.ac.kr>
2023-12-24 22:29:15 +0900

    Show navigation bar in application

--- app/src/main/java/com/kael21ce/sleepanalysisandroid/MainActivity.java
+++ app/src/main/java/com/kael21ce/sleepanalysisandroid/MainActivity.java
@@ -216,11 +216,12 @@ public class MainActivity extends AppCompatActivity {
                 editor.apply();
             }
         }
-
+        /*
         //Hide navigation bar
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+         */
 
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
