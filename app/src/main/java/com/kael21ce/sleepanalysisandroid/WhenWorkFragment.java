57baef8fd53cf3e37461cc166051320a5cbaf91a
HyeongJun <kael21ce@kaist.ac.kr>
2024-10-24 17:31:32 +0900

    Send work data in Onboarding page

--- app/src/main/java/com/kael21ce/sleepanalysisandroid/WhenWorkFragment.java
+++ app/src/main/java/com/kael21ce/sleepanalysisandroid/WhenWorkFragment.java
@@ -21,10 +21,20 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.kael21ce.sleepanalysisandroid.data.DataSurvey;
+import com.kael21ce.sleepanalysisandroid.data.RetrofitAPI;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class WhenWorkFragment extends Fragment {
@@ -394,6 +404,8 @@ public class WhenWorkFragment extends Fragment {
                     mainActivity.setWorkOnset(workOnset.getTime());
                     mainActivity.setWorkOffset(workOffset.getTime());
 
+                    sendSurvey(sleepOnset.getTime(), workOnset.getTime(), workOffset.getTime());
+
                     mainActivity.finish();
                     startActivity(new Intent(mainActivity, SplashActivity.class));
                 }else{
@@ -431,5 +443,57 @@ public class WhenWorkFragment extends Fragment {
         int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
 
         return dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY;
+    }
+
+    private void sendSurvey(long sleep_onset, long work_onset, long work_offset){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://sleep-math.com/sleepapp/")
+                // as we are sending data in json format so
+                // we have to add Gson converter factory
+                .addConverterFactory(GsonConverterFactory.create())
+                // at last we are building our retrofit builder.
+                .build();
+        RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class);
+        SharedPreferences sharedPref = getActivity().getSharedPreferences("SleepWake", Context.MODE_PRIVATE);
+        String username = sharedPref.getString("User_Name", "tester33");
+        long time = System.currentTimeMillis();
+
+        DataSurvey survey = new DataSurvey(username, sleep_onset, work_onset, work_offset, -1, time);
+        Call<DataSurvey> call = retrofitAPI.createSurvey(survey);
+        call.enqueue(new Callback<DataSurvey>() {
+            @Override
+            public void onResponse(Call<DataSurvey> call, Response<DataSurvey> response) {
+                // this method is called when we get response from our api.
+                Locale currentLocale = Locale.getDefault();
+                String language = currentLocale.getLanguage();
+                if(response.code() <= 300) {
+                    if (language.equals("ko")) {
+                        Toast.makeText(getActivity(), "데이터가 전송되었습니다", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getActivity(), "Data added to API", Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+                    if (language.equals("ko")) {
+                        Toast.makeText(getActivity(), "데이터 전송에 실패했습니다", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getActivity(), "Data sending failed", Toast.LENGTH_SHORT).show();
+                    }
+                    // we are getting response from our body
+                    // and passing it to our modal class.
+                    DataSurvey responseFromAPI = response.body();
+
+                    // on below line we are getting our data from modal class and adding it to our string.
+                    String responseString = "Response Code : " + response.code() + "\nName : " + "\n";
+                    Log.v("RESPONSE", responseString);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DataSurvey> call, Throwable t) {
+                // setting text to our text view when
+                // we get error response from API.
+                Log.v("ERROR", "Error found is : " + t.getMessage());
+            }
+        });
     }
 }
